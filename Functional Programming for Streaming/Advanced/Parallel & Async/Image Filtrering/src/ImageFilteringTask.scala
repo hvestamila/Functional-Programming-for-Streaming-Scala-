0d8793f95f7e5acdf80d9b61abf8e39a6ed2474c
Tamila Heptinh <hves.tamila@gmail.com>
2021-11-21 18:09:16 +0200

    Added Advanced

--- Functional Programming for Streaming/Advanced/Parallel & Async/Image Filtrering/src/ImageFilteringTask.scala
+++ Functional Programming for Streaming/Advanced/Parallel & Async/Image Filtrering/src/ImageFilteringTask.scala
@@ -1,6 +1,42 @@
 import java.util.concurrent.RecursiveTask
 
 object ImageFilteringTask {
-  def filterImage(filter: Array[Array[Int]] => (Int, Int) => Int, windowSize: Int)(imageData: Array[Array[Int]]): Array[Array[Int]] = ???
+  def filterImage(filter: Array[Array[Int]] => (Int, Int) => Int, windowSize: Int)(imageData: Array[Array[Int]]): Array[Array[Int]] = {
+    FilteringTask(filter(imageData), windowSize, 0, imageData(0).length, 0, imageData.length)()
+  }
 
+
+  case class FilteringTask(filter: (Int, Int) => Int, windowSize: Int, collStart: Int, collEnd: Int, rowStart: Int, rowEnd: Int) extends RecursiveTask[Array[Array[Int]]] {
+
+    def apply(): Array[Array[Int]] = {
+      val rowCnt = rowEnd - rowStart
+      val collCnt = collEnd - collStart
+
+      if (collCnt <= windowSize && rowCnt <= windowSize) {
+        Array.tabulate[Int](rowCnt, collCnt)((row, coll) => filter(rowStart + row, collStart + coll))
+      } else if (rowCnt > windowSize) {
+        val partSize = rowCnt / 2
+
+        val asyncSubtask = FilteringTask(filter, windowSize, collStart, collEnd, rowStart + partSize, rowEnd).fork()
+        Array.concat(
+          FilteringTask(filter, windowSize, collStart, collEnd, rowStart, rowStart + partSize)(),
+          asyncSubtask.join()
+        )
+      } else {
+        val partSize = collCnt / 2
+
+        val asyncSubtask = FilteringTask(filter, windowSize, collStart + partSize, collEnd, rowStart, rowEnd).fork()
+
+        val part1Result = FilteringTask(filter, windowSize, collStart, collStart + partSize, rowStart, rowEnd)()
+        val part2Result = asyncSubtask.join()
+
+        for (i <- 0 until rowCnt)
+          part1Result.update(i, Array.concat(part1Result(i), part2Result(i)))
+
+        part1Result
+      }
+    }
+
+    override def compute(): Array[Array[Int]] = apply()
+  }
 }
--- Functional Programming for Streaming/Advanced/Parallel & Async/Wiki Integration/src/WikiIntegrationTask.scala
+++ Functional Programming for Streaming/Advanced/Parallel & Async/Wiki Integration/src/WikiIntegrationTask.scala
@@ -14,7 +14,27 @@ import scala.xml.NodeSeq
 
 object WikiIntegrationTask {
 
-  def retrieveLatestChanges(): Future[Seq[WikiChange]] = ???
+  def retrieveLatestChanges(): Future[Seq[WikiChange]] = {
+    implicit val system = ActorSystem(Behaviors.empty, "SingleRequest")
+    // needed for the future flatMap/onComplete in the end
+    implicit val executionContext = system.executionContext
+
+
+    val request = HttpRequest(
+      method = HttpMethods.GET,
+      uri = "https://en.wikipedia.org/w/api.php?action=feedrecentchanges"
+    )
+
+    def sendGet(): Future[Seq[WikiChange]] = {
+      val responseFuture: Future[Seq[WikiChange]] = Http().singleRequest(request)
+        .map(_.entity)
+        .flatMap(Unmarshal(_).to[NodeSeq])
+        .map(a => (a \ "channel" \ "item").map(node => WikiChange((node \ "title").text, Uri((node \ "link").text), Uri((node \ "guid").text), (node \ "description").text, LocalDateTime.from(DateTimeFormatter.RFC_1123_DATE_TIME.parse((node \ "pubDate").text)))))
+
+      responseFuture
+    }
+    sendGet()
+  }
 
   case class WikiChange(title: String, link: Uri, guid: Uri, description: String, pubDate: LocalDateTime)
 
